e97c252bed84f4efe03570e632567444afa7336d
Wenyu Pu <puw@miamioh.edu>
2017-11-03 21:02:32 -0400

    New LoginPanel

--- 201/LoginPanel.java
+++ 201/LoginPanel.java
@@ -8,7 +8,11 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 
 public class LoginPanel {
@@ -16,6 +20,7 @@ public class LoginPanel {
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
+	private RandomAccessFile in;
 
 	/**
 	 * Launch the application.
@@ -47,59 +52,87 @@ public class LoginPanel {
 		
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 592, 453);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblUserName = new JLabel("User name");
-		lblUserName.setBounds(34, 80, 72, 18);
-		frame.getContentPane().add(lblUserName);
-		
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(34, 117, 72, 18);
-		frame.getContentPane().add(lblPassword);
-		
-		textField = new JTextField();
-		textField.setBounds(120, 77, 187, 24);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(120, 114, 187, 24);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setForeground(Color.BLACK);
+		panel.setBounds(0, 0, 574, 406);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
 		
 		JButton btnLogIn = new JButton("LOG IN");
+		btnLogIn.setBackground(Color.LIGHT_GRAY);
+		btnLogIn.setForeground(Color.BLACK);
+		btnLogIn.setFont(new Font("宋体", Font.BOLD, 15));
+		btnLogIn.setBounds(362, 276, 113, 27);
+		panel.add(btnLogIn);
+		
+		JButton btnCreateAccout = new JButton("Create Accout");
+		btnCreateAccout.setBackground(Color.LIGHT_GRAY);
+		btnCreateAccout.setFont(new Font("宋体", Font.BOLD, 15));
+		btnCreateAccout.setBounds(404, 366, 156, 27);
+		panel.add(btnCreateAccout);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(328, 211, 187, 24);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setBounds(328, 156, 187, 24);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblUserName = new JLabel("User name");
+		lblUserName.setFont(new Font("宋体", Font.BOLD, 15));
+		lblUserName.setBounds(222, 159, 92, 18);
+		panel.add(lblUserName);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("宋体", Font.BOLD, 15));
+		lblPassword.setBounds(222, 214, 72, 18);
+		panel.add(lblPassword);
+		
+		JLabel lblLogInPage = new JLabel("LOG IN ");
+		lblLogInPage.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogInPage.setForeground(Color.BLUE);
+		lblLogInPage.setBounds(335, 74, 158, 51);
+		panel.add(lblLogInPage);
+		lblLogInPage.setFont(new Font("华文琥珀", Font.BOLD, 40));
+		btnCreateAccout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		btnLogIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			
-//				User user = new User();
-//				ArrayList<String> n = user.getName();
-//				ArrayList<String> p = user.getName();
-//			 if(n.contains(textField)){
-//				 int location = n.indexOf(textField);
-//				if(textField_1.equals(p.get(location))){
-//					Test nw = new Test();
-//					nw.NewScreen();
-//				}
-//			 }
 				
-				Test nw = new Test();
-				nw.NewScreen();
+			
+				User user = new User();
+				try{
+					in = new RandomAccessFile("UserData.txt","rw");
+					long length = in.length();
+					for(int i = 0; i < length ; i++){
+						if(in.readLine().equals(textField.getText())){
+							if(in.readLine().equals(textField_1.getText())){
+								break;
+							}
+						}
+					}
+					MainPage nw = new MainPage();
+					nw.NewScreen();
+					
+				}
+				catch(Exception p) {}
+				
+				
+			
+				
 				
 				
 			}
 		});
-		btnLogIn.setBounds(155, 162, 113, 27);
-		frame.getContentPane().add(btnLogIn);
-		
-		JButton btnCreateAccout = new JButton("Create Accout");
-		btnCreateAccout.setBounds(281, 213, 137, 27);
-		frame.getContentPane().add(btnCreateAccout);
-		
-		JLabel lblLogInPage = new JLabel("LOG IN ");
-		lblLogInPage.setFont(new Font("宋体", Font.BOLD, 34));
-		lblLogInPage.setBounds(137, 13, 137, 51);
-		frame.getContentPane().add(lblLogInPage);
 	}
 }
